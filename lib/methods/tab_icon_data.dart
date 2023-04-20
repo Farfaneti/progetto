6a7f347416fb815a4e732d5ba51d4e08445ca5ab
GiuliaMarigo <giulia.marigo@studenti.unipd.it>
2023-04-20 21:18:51 +0200

    bottom bar images update

--- lib/methods/tab_icon_data.dart
+++ lib/methods/tab_icon_data.dart
@@ -19,29 +19,29 @@ class TabIconData {
   // da cambiare le immagini/icone
   static List<TabIconData> tabIconsList = <TabIconData>[
     TabIconData(
-      imagePath: 'assets/fitness_app/tab_1.png',
-      selectedImagePath: 'assets/fitness_app/tab_1s.png',
+      imagePath: 'assets/tab_1.png',
+      selectedImagePath: 'assets/tab_1s.png',
       index: 0,
       isSelected: true,
       animationController: null,
     ),
     TabIconData(
-      imagePath: 'assets/fitness_app/tab_2.png',
-      selectedImagePath: 'assets/fitness_app/tab_2s.png',
+      imagePath: 'assets/tab_2.png',
+      selectedImagePath: 'assets/tab_2s.png',
       index: 1,
       isSelected: false,
       animationController: null,
     ),
     TabIconData(
-      imagePath: 'assets/fitness_app/tab_3.png',
-      selectedImagePath: 'assets/fitness_app/tab_3s.png',
+      imagePath: 'assets/tab_3.png',
+      selectedImagePath: 'assets/tab_3s.png',
       index: 2,
       isSelected: false,
       animationController: null,
     ),
     TabIconData(
-      imagePath: 'assets/fitness_app/tab_4.png',
-      selectedImagePath: 'assets/fitness_app/tab_4s.png',
+      imagePath: 'assets/tab_4.png',
+      selectedImagePath: 'assets/tab_4s.png',
       index: 3,
       isSelected: false,
       animationController: null,
